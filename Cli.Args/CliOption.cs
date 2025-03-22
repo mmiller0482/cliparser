7b6e5ee72f24dd7508e51805543091e5d41c584c
matthewmiller <mmiller0482@gmail.com>
2025-03-22 19:42:16 -0400

    Documented CliOption

--- Cli.Args/CliOption.cs
+++ Cli.Args/CliOption.cs
@@ -1,10 +1,29 @@
 namespace Cli.Args;
 
+/// <summary>
+/// Specifies directives for parsing CLI args into a concrete class.
+/// Used in help generator to display help.
+/// </summary>
 [AttributeUsage(AttributeTargets.Property)]
 public class CliOptionAttribute : Attribute
 {
+    // TODO: What are we going to do about 'subcommands' ? Future issue.
+    /// <summary>
+    /// The shorthand specifier for a keyword argument.
+    /// Example. -h for help.
+    /// </summary>
     public string ShortName { get; }
+    
+    /// <summary>
+    /// The longhand specifier for a keyword argument.
+    /// Example. --help for help
+    /// </summary>
     public string LongName { get; }
+    
+    /// <summary>
+    /// Specifies whether the argument is required.
+    /// Default -- false.
+    /// </summary>
     public bool Required { get; set; }
 
     public CliOptionAttribute(string shortName, string longName)
